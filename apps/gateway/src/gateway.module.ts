bb4cab8b23966a8597ae1ff1f1762eddd196fe66
Glockers <megamaxchanel@gmail.com>
2023-08-23 16:56:05 +0300

    feat: add dockerfiles for microservices

--- apps/gateway/src/gateway.module.ts
+++ apps/gateway/src/gateway.module.ts
@@ -2,15 +2,15 @@ import { Module } from '@nestjs/common';
 import { GatewayController } from './gateway.controller';
 import { GatewayService } from './gateway.service';
 import { ConfigModule } from '@nestjs/config';
-import { DatabaseModule } from '@app/common';
+// import { DatabaseModule } from '@app/common';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: './apps/gateway/.env'
-    }),
-    DatabaseModule
+    })
+    // DatabaseModule
   ],
   controllers: [GatewayController],
   providers: [GatewayService]
