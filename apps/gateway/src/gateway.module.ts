38f70487c988fd424003b6702e61620ddc4f43ce
Glockers <megamaxchanel@gmail.com>
2023-08-23 09:22:05 +0300

    refact: refact database code

--- apps/gateway/src/gateway.module.ts
+++ apps/gateway/src/gateway.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 import { GatewayController } from './gateway.controller';
 import { GatewayService } from './gateway.service';
 import { ConfigModule } from '@nestjs/config';
-import { DatabaseModule } from '@app/common/database/database.module';
+import { DatabaseModule } from '@app/common';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      isGlobal: true
+      isGlobal: true,
+      envFilePath: './apps/gateway/.env'
     }),
     DatabaseModule
   ],
--- libs/common/src/database/database.module.ts
+++ libs/common/src/database/database.module.ts
@@ -2,18 +2,22 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { Meetup } from './entities/meetup.entity';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'root',
-      database: 'meetup',
-      entities: [User, Meetup],
-      synchronize: true
+    TypeOrmModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('TYPEORM_HOST'),
+        port: +configService.get('TYPEORM_PORT'),
+        username: configService.get('TYPEORM_USERNAME'),
+        password: configService.get('TYPEORM_PASSWORD'),
+        database: configService.get('TYPEORM_DATABASE'),
+        entities: [User, Meetup],
+        synchronize: true
+      }),
+      inject: [ConfigService]
     })
   ]
 })
