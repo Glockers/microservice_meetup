eb87c258efef551ea5d2241524b2ef4157537617
Glockers <megamaxchanel@gmail.com>
2023-10-03 15:52:27 +0300

    feat: add migration for auth

--- apps/auth/src/app.module.ts
+++ apps/auth/src/app.module.ts
@@ -1,8 +1,11 @@
 import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
 import { mergedConfigValidationSchema } from './schemas/main';
-import { AuthModule } from './modules/auth/auth.module';
 import { FirabaseModule } from './modules/firebase/firebase.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { RmqModule } from '@app/common';
+import { AuthModule } from './modules/auth/auth.module';
+import { dataSourceOptions } from '../db/typeorm.config';
 
 @Module({
   imports: [
@@ -11,6 +14,10 @@ import { FirabaseModule } from './modules/firebase/firebase.module';
       validationSchema: mergedConfigValidationSchema,
       envFilePath: './apps/auth/.env'
     }),
+    TypeOrmModule.forRootAsync({
+      useFactory: () => dataSourceOptions
+    }),
+    RmqModule,
     AuthModule,
     FirabaseModule
   ]
